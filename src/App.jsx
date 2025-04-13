bfcaacca14ea1c710d47964d9af4beab6e82698b
soponalways <soponalways@gmail.com>
2025-04-13 21:03:50 +0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+import DaisyNav from './components/daisyNav/DaisyNav';
+const App = () => {
+  return (
+    <div>
+      <header>
+        <DaisyNav></DaisyNav>
+      </header>
+    </div>
+  );
+};
+
+export default App;
