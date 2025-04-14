796d1afdbc9fcb64fc182d4312c083237ae72a6c
soponalways <soponalways@gmail.com>
2025-04-14 17:16:44 +0600

    Daisy Pricing Card Done

--- src/components/DaisyPricingCard/DaisyPricingCard.jsx
+++ src/components/DaisyPricingCard/DaisyPricingCard.jsx
@@ -1,39 +1,22 @@
 import React from 'react';
 
-const DaisyPricingCard = () => {
+const DaisyPricingCard = ({ pricing }) => {
+    const {title, price, features, popular} = pricing; 
     return (
-        <div className="card w-96 bg-base-100 shadow-sm">
-            <div className="card-body">
-                <span className="badge badge-xs badge-warning">Most Popular</span>
+        <div className="card w-96 bg-base-100 shadow-sm mx-auto">
+            <div className="card-body flex">
+                {popular && <span className="badge badge-xs badge-warning">Most Popular</span>}
                 <div className="flex justify-between">
-                    <h2 className="text-3xl font-bold">Premium</h2>
-                    <span className="text-xl">$29/mo</span>
+                    <h2 className="text-3xl font-bold">{title} </h2>
+                    <span className="text-xl">{price}</span>
                 </div>
-                <ul className="mt-6 flex flex-col gap-2 text-xs">
-                    <li>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-                        <span>High-resolution image generation</span>
-                    </li>
-                    <li>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-                        <span>Customizable style templates</span>
-                    </li>
-                    <li>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-                        <span>Batch processing capabilities</span>
-                    </li>
-                    <li>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-                        <span>AI-driven image enhancements</span>
-                    </li>
-                    <li className="opacity-50">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-                        <span className="line-through">Seamless cloud integration</span>
-                    </li>
-                    <li className="opacity-50">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-                        <span className="line-through">Real-time collaboration tools</span>
-                    </li>
+                <ul className="mt-6 flex flex-col gap-2 text-xs flex-1 ">
+                    {
+                        features.map((feature, index) => <li key={index}>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>{feature}</span>
+                        </li>)
+                    }
                 </ul>
                 <div className="mt-6">
                     <button className="btn btn-primary btn-block">Subscribe</button>
--- src/components/pricingOptions/PricingOptions.jsx
+++ src/components/pricingOptions/PricingOptions.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import PricingCard from '../pricingCard/PricingCard';
+import DaisyPricingCard from '../DaisyPricingCard/DaisyPricingCard';
 
 const PricingOptions = ({ pricingPromise }) => {
     const pricingData = use(pricingPromise); 
@@ -7,9 +8,14 @@ const PricingOptions = ({ pricingPromise }) => {
     return (
         <div className='space-y-3 mt-10 mx-4'>
             <h2 className="text-5xl font-bold text-center underline">Get Our MemberShip </h2>
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6 '>
+            {/* <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 '>
                 {
                     pricingData.map(pricing => <PricingCard key={pricing.id} pricing={pricing}></PricingCard>)
+                }
+            </div> */}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 '>
+                {
+                    pricingData.map(pricing => <DaisyPricingCard pricing={pricing} key={pricing.id}></DaisyPricingCard>)
                 }
             </div>
         </div>
